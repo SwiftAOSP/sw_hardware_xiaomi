RyzenOS/hardware_xiaomi

aidl: power: Adapt ndk backend with Android 13.0.0 Release
Reference:
https://android.googlesource.com/device/google/gs101/+/c77442bde506320818b3302262f0efca5bf9c869

aidl/power-libperfmgr/Android.mk
@@ -24,7 +24,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SHARED_LIBRARIES := \
-    android.hardware.power-V1-ndk_platform \
+    android.hardware.power-V1-ndk \
     libbase \
     libbinder_ndk \
     libcutils \
@@ -32,7 +32,7 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libperfmgr \
     libutils \
-    pixel-power-ext-V1-ndk_platform
+    pixel-power-ext-V1-ndk
 
 LOCAL_SRC_FILES := \
     service.cpp \

SwiftAOSP/sw_hardware_xiaomi

aidl/power-libperfmgr/Android.mk
@@ -24,7 +24,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SHARED_LIBRARIES := \
-    android.hardware.power-V2-ndk_platform \
+    android.hardware.power-V2-ndk \
     libbase \
     libbinder_ndk \
     libcutils \
@@ -33,7 +33,7 @@ LOCAL_SHARED_LIBRARIES := \
     libperfmgr \
     libprocessgroup \
     libutils \
-    pixel-power-ext-V1-ndk_platform
+    pixel-power-ext-V1-ndk
 
 LOCAL_SRC_FILES := \
     service.cpp \